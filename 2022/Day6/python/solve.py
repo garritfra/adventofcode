ec0aa5547e456d95fb1162808120da59c98ec4f9
Garrit Franke <garrit@slashdev.space>
2022-12-06 07:45:13 +0100

    Solve 2022 Day 6 in python

---
+++ 2022/Day6/python/solve.py
@@ -0,0 +1,19 @@
+import os
+import re
+
+
+def solve(line, min_different):
+    for i, _ in enumerate(line):
+        if i < min_different - 1: continue
+
+        chars = line[i:i+min_different]
+        if len(set(chars)) == min_different:
+            return i + min_different
+
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '../input.txt')
+with open(filename) as f:
+    line = f.readline()
+
+print(solve(line, 4))
+print(solve(line, 14))
